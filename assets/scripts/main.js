7954cf077e8d80a1b7f0c4f592254b8610ee69ce
YoanMorel <yoan.morel@live.fr>
2019-04-08 00:18:09 +0200

    Repos des exos part2

---
+++ assets/scripts/main.js
@@ -0,0 +1,55 @@
+// Exercice 1
+// let myImage = document.querySelector('img');
+//
+// myImage.addEventListener('mouseover', function() {
+//     let mySrc = myImage.getAttribute('src');
+//     if (mySrc === 'assets/img/image1.jpg') {
+//       myImage.setAttribute('src', 'assets/img/image1_2.jpg');
+//     }
+// });
+
+// Exercice 2
+// let myInput = document.querySelector('input');
+//
+// myInput.addEventListener('blur', function() {
+//   alert('Merci de votre participation');
+// });
+
+// Exercice 3
+// let targetInput = document.querySelector('input');
+//
+// targetInput.addEventListener('keyup', function () {
+//   let myInput = document.getElementById('lastname').value;
+//   alert(myInput);
+// });
+
+// Exercice 4
+// let btnRaz = document.querySelector('button');
+//
+// btnRaz.addEventListener('click', function() {
+//   document.getElementById('input').value = '';
+//   document.getElementById('firstname').value = '';
+//   document.getElementById('city').value = '';
+// });
+
+// Exercice 5
+
+// var myImg = document.getElementsByTagName('img');
+var myImg = document.querySelectorAll('img');
+
+var imgIndex = function(index) {
+  myImg[index].setAttribute('src', 'assets/img/image' + (index + 1) + '_2.jpg');
+}
+
+myImg.forEach(function(img, i){
+  img.addEventListener('mouseover', function(){
+    imgIndex(i);
+  });
+});
+
+// for(let i = 0; i < myImg.length; i++) {
+//   let mySrc = myImg[i];
+//   mySrc.addEventListener('mouseover', function() {
+//     imgIndex(i);
+//   });
+// }
